85ef3aecdca8ccc53db6114caef98616955ae8e0
Kevin Doveton <kevindoveton@me.com>
2018-05-15 19:24:03 +0930

    finally defeated the image download

--- CSharp-React-Twitter/Controllers/User.cs
+++ CSharp-React-Twitter/Controllers/User.cs
@@ -5,26 +5,31 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using CSharpReactTwitter.Models;
+using System.IO;
 using System.Net;
+using System.Net.Http;
+
+//using System.Web.Http;
+
 namespace CSharpReactTwitter.Controllers {
-  [Route("api/[controller]")]
-  public class User : Controller {
+
   
-    // GET api/values/5
-    [HttpGet("{username}")]
-    public string Get(string username) {
-      Random rand = new Random(username.GetHashCode());
+	[Route("api/[controller]")]
+	 public class User : ControllerBase {
+		// GET api/values/5
+		[HttpGet("{username}")]
+		public FileContentResult Get(string username) {
+			Random rand = new Random(username.GetHashCode());
       string color = rand.Next()
                          .ToString("X")
                          .Substring(0, 6);
 
+      
+
       using (WebClient client = new WebClient()) {
-        client.DownloadFile("https://cdn.sstatic.net/Sites/stackoverflow/img/404.svg", "image.png");
-
-        Console.Write("hello");
-        return "Hello";
+				var dataBytes = client.DownloadData("https://placehold.it/100x100/" + color);
+				return new FileContentResult(dataBytes, "image/png");
       }
-    }
-
-  }
+		}
+	}
 }
