0a08235079d5b7a9d87fbe0a500aa60021201112
Kevin Doveton <kevindoveton@me.com>
2018-05-17 20:31:50 +0930

    fix up things

--- CSharp-React-Twitter/Controllers/Newsfeed.cs
+++ CSharp-React-Twitter/Controllers/Newsfeed.cs
@@ -20,7 +20,6 @@ namespace CSharpReactTwitter.Controllers {
     
     // GET api/newsfeed/
     [HttpGet]
-<<<<<<< HEAD
     public async Task<IActionResult> Get() {
 
       // get the following
@@ -37,13 +36,6 @@ namespace CSharpReactTwitter.Controllers {
 			var response = tweets.Select(x => x);
 
       return Ok(response);
-=======
-    public string Get() {
-			return JsonConvert.SerializeObject(new {
-        tweets = db.GetNewsfeedForUser(0),
-        user = db.GetUserById(0)
-      });
->>>>>>> master
     }
     
   }
